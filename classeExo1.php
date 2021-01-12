9dac2c64c6cacaab273dfe1ed9cd108f4bbc6c50
L.Bernard <lbernard@la-providence.net>
2021-01-12 14:57:54 +0100

    first commit

---
+++ classeExo1.php
@@ -0,0 +1,22 @@
+<?php
+ class user{
+
+private $_Nom;
+private $_Prenom;
+
+
+
+public function AfficherUser()
+{
+echo"je suis un user";
+
+}
+
+
+
+ }
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<?php include("classeExo1.php");?>
+</head>
+<body>
+    
+<?php 
+ $Objetuser = new user("chloé");
+ $Objetuser->AfficherUser( );
+
+
+?>
+</body>
+</html>
